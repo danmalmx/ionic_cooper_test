0f1463fc50b7aeb7c3458977b32dc021a86d53d4
danmalmx <danmalmx@gmail.com>
2018-10-09 20:48:17 +0200

    [WIP] creating the controller - 1 test still breaking

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'should have valid Factory' do
-    expect(create(:user)).to be_valid
+    it 'should have valid Factory' do
+      expect(create(:user)).to be_valid
+    end
 
     describe 'Database table' do
         it { is_expected.to have_db_column :id }
@@ -27,33 +28,28 @@ RSpec.describe User, type: :model do
         it { is_expected.to have_db_column :tokens }
         it { is_expected.to have_db_column :created_at }
         it { is_expected.to have_db_column :updated_at }
-    
-    describe 'Validations' do
-        it { is_expected.to validate_presence_of(:email) }
-        it { is_expected.to validate_confirmation_of(:password) }
-    
-        context 'should not have an invalid email address' do
-            emails = ['asdf@ ds.com', '@example.com', 'test me @yahoo.com',
-                    'asdf@example', 'ddd@.d. .d', 'ddd@.d']
-    
-            emails.each do |email|
-            it { is_expected.not_to allow_value(email).for(:email) }
-            end
-        end
-    
-        context 'should have a valid email address' do
-            emails = ['asdf@ds.com', 'hello@example.uk', 'test1234@yahoo.si',
-                    'asdf@example.eu']
-    
-            emails.each do |email|
-            it { is_expected.to allow_value(email).for(:email) }
-            end
-        end
   end
 
-    describe 'Relations' do
-        it { is_expected.to belong_to :user }
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_confirmation_of(:password) }
+
+    context 'should not have an invalid email address' do
+      emails = ['asdf@ ds.com', '@example.com', 'test me @yahoo.com',
+                'asdf@example', 'ddd@.d. .d', 'ddd@.d']
+
+      emails.each do |email|
+        it { is_expected.not_to allow_value(email).for(:email) }
+      end
     end
+
+    context 'should have a valid email address' do
+      emails = ['asdf@ds.com', 'hello@example.uk', 'test1234@yahoo.si',
+                'asdf@example.eu']
+
+      emails.each do |email|
+        it { is_expected.to allow_value(email).for(:email) }
+      end
     end
-end
-end
+  end
+end             
